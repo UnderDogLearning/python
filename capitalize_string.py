c689c864ee6d7bfa6507fa61add4be5e7c534308
UnderDogLearning <78521523+UnderDogLearning@users.noreply.github.com>
2021-02-10 17:03:48 -0600

    Create capitalize_string.py

---
+++ capitalize_string.py
@@ -0,0 +1,12 @@
+def capitalize(phrase):
+    if type(phrase) is str:
+        return(phrase.capitalize())
+        
+        
+        
+"""Capitalize first letter of first word of phrase.
+        >>> capitalize('python')
+        'Python'
+        >>> capitalize('only first word')
+        'Only first word'
+"""
