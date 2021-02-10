6bdb9726be800876c8f420da7992ba5f3b324494
UnderDogLearning <78521523+UnderDogLearning@users.noreply.github.com>
2021-02-10 12:53:17 -0600

    Create product_addition_sub_division.py

---
+++ product_addition_sub_division.py
@@ -0,0 +1,37 @@
+def product(a, b):
+    """Return product of a and b.
+        >>> product(2, 2)
+        4
+        >>> product(2, -2)
+        -4
+    """
+    return a * b
+    
+def addition(a, b):
+    """Return product of a and b.
+        >>> product(2, 2)
+        4
+        >>> product(2, -2)
+        -4
+    """
+    return a + b
+    
+def subtraction(a, b):
+    """Return product of a and b.
+        >>> product(2, 2)
+        4
+        >>> product(2, -2)
+        -4
+    """
+    return a - b
+
+def division(a, b):
+    """Return product of a and b.
+        >>> product(2, 2)
+        4
+        >>> product(2, -2)
+        -4
+    """
+    return a / b
+    
+     
